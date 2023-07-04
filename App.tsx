2cf5270551c6fbf05ca9df2e7d5463cd8d565898
Gaangb <angelob4stos@gmail.com>
2023-07-04 17:45:36 -0300

    Criando projeto dio lanterna verde com react-native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
+
 
 export default function App() {
+  const [ActivateState, setActivateState] = useState(false);
+
+
+  function handleSymbol(){
+    setActivateState((oldValue:boolean) =>{
+      return !oldValue;
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ActivateState? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={()=>{handleSymbol}}>
+        <Image 
+          source={ActivateState? symbolOn : symbolOff}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerOff:{
+    flex: 1,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
